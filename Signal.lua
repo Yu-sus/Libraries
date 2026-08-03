cce9cb999846d070e5f690c27dcc93f02061f809
Yu-sus <gemapliskodak@proton.me>
2026-08-03 14:09:18 -0300

    Update Signal.lua

--- Signal.lua
+++ Signal.lua
@@ -1,41 +1,107 @@
 local Signal = {}
 Signal.__index = Signal
 
+local Connection = {}
+Connection.__index = Connection
+
 function Signal.new()
 	return setmetatable({
-		_connections = {},
+		_head = nil,
+		_destroyed = false,
 	}, Signal)
 end
 
-function Signal:Connect(callback)
-	local connection = {
-		Connected = true,
-	}
+function Connection:Disconnect()
+	if not self.Connected then
+		return
+	end
 
-	function connection:Disconnect()
-		if not self.Connected then
+	self.Connected = false
+
+	local signal = self._signal
+
+	if signal._head == self then
+		signal._head = self._next
+		return
+	end
+
+	local node = signal._head
+
+	while node do
+		if node._next == self then
+			node._next = self._next
 			return
 		end
 
-		self.Connected = false
+		node = node._next
 	end
+end
 
-	connection.Callback = callback
-	table.insert(self._connections, connection)
+function Signal:Connect(callback)
+	assert(type(callback) == "function", "callback must be a function")
+
+	local connection = setmetatable({
+		Connected = true,
+		Callback = callback,
+		_signal = self,
+		_next = self._head,
+	}, Connection)
+
+	self._head = connection
+
+	return connection
+end
+
+function Signal:Once(callback)
+	local connection
+
+	connection = self:Connect(function(...)
+		connection:Disconnect()
+		callback(...)
+	end)
 
 	return connection
 end
 
 function Signal:Fire(...)
-	for _, connection in ipairs(self._connections) do
-		if connection.Connected then
-			task.spawn(connection.Callback, ...)
+	local node = self._head
+
+	while node do
+		if node.Connected then
+			task.spawn(node.Callback, ...)
 		end
+
+		node = node._next
 	end
 end
 
+function Signal:Wait()
+	local thread = coroutine.running()
+
+	local connection
+
+	connection = self:Connect(function(...)
+		connection:Disconnect()
+		task.spawn(thread, ...)
+	end)
+
+	return coroutine.yield()
+end
+
+function Signal:DisconnectAll()
+	local node = self._head
+
+	while node do
+		node.Connected = false
+		node = node._next
+	end
+
+	self._head = nil
+end
+
 function Signal:Destroy()
-	table.clear(self._connections)
+	self:DisconnectAll()
+	self._destroyed = true
 end
 
 return Signal
